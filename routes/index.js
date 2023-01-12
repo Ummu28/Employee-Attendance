023457ef2c238ab18f6724212a55a3353a308422
Ummu28 <ummuaiman3019@gmail.com>
2023-01-12 20:48:18 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
 	return res.render('index.ejs');
-}); 
+});
 
 router.get('/dashboard', function (req, res, next) {
 	return res.render('dashboard.ejs');
@@ -27,15 +27,15 @@ router.get('/form', function (req, res, next) {
 
 router.get('/employee', function (req, res, next) {
 	User.find((err, docs) => {
-        if (!err) {
-            res.render("admin/employee.ejs", { 
+		if (!err) {
+			res.render("admin/employee.ejs", {
 				data: docs
 			});
-        } else {
-            console.log(err);
-        }
-    });
-	
+		} else {
+			console.log(err);
+		}
+	});
+
 });
 
 router.get('/attendance', function (req, res, next) {
@@ -86,8 +86,22 @@ router.get('/adminLeave', function (req, res, next) {
 	return res.render('admin/adminLeave.ejs');
 });
 
-router.get('/editProfile', function (req, res, next) {
-	return res.render('admin/editProfile.ejs');
+
+
+router.get('/editProfile/:id', function (req, res, next) { 
+	
+	User.findOne(({_id: req.params.id}), (err, docs) => {
+		if (!err) {
+			console.log(docs);
+			res.render("admin/editProfile.ejs", {
+				data: docs
+			});
+		} else {
+			console.log('Failed to retrieve the booking List: ' + err);
+		}
+	});
+
+	
 });
 
 router.get('/adminReport', function (req, res, next) {
@@ -99,75 +113,82 @@ router.get('/clock', function (req, res, next) {
 	var dt = dateTime.create();
 	//var formatted = dt.format('Y-m-d H:M:S');
 	var formatted = dt.format('d-m-Y   H:M');
-	
-	
-	return res.render('clock.ejs', {cur_time: formatted});
-	
+
+
+	return res.render('clock.ejs', {
+		cur_time: formatted
+	});
+
 });
 
 //https://github.com/nooraiman/Sales-Record-System/blob/main/products/index.php
 
 //router.post('/clock', (req, res)=>{
-	
-	//var dateTime = require('node-datetime');
-	//var dt = dateTime.create();
-	//var formatted = dt.format('Y-m-d H:M:S');
 
-	// Save current time when user login into the database
-	 //res.render('clock.ejs',  {cur_time: formatted});
-	//res.json({"cur_time": formatted})
+//var dateTime = require('node-datetime');
+//var dt = dateTime.create();
+//var formatted = dt.format('Y-m-d H:M:S');
+
+// Save current time when user login into the database
+//res.render('clock.ejs',  {cur_time: formatted});
+//res.json({"cur_time": formatted})
 
 //});
 
 
 
-    
+
 //app.listen(3000, () => console.log(`App listening on port 3000`))
 //---------------------------------------------------------------------------------
 
 //-----------------------------------------ADD EMPLOYEE -----------------------------
 
-router.post('/addEmp', function(req, res, next) { //registration
-    console.log(req.body);
-    var personInfo = req.body;
-    if(!personInfo.name || !personInfo.email || !personInfo.passwd || !personInfo.passwdConf 
-		|| !personInfo.dob || !personInfo.gender || !personInfo.city || !personInfo.noHp || !personInfo.dateJoin){
-        res.send();
-    } else {
-        if (personInfo.passwd == personInfo.passwdConf) {
-            User.findOne({email: personInfo.email}, (err,result)=>{
-				
-                if(!result) {
-                    var newPerson = new User
-					({
+router.post('/addEmp', function (req, res, next) { //registration
+	console.log(req.body);
+	var personInfo = req.body;
+	if (!personInfo.name || !personInfo.email || !personInfo.passwd || !personInfo.passwdConf ||
+		!personInfo.dob || !personInfo.gender || !personInfo.city || !personInfo.noHp || !personInfo.dateJoin) {
+		res.send();
+	} else {
+		if (personInfo.passwd == personInfo.passwdConf) {
+			User.findOne({
+				email: personInfo.email
+			}, (err, result) => {
+
+				if (!result) {
+					var newPerson = new User({
 						username: personInfo.username,
-                        email:personInfo.email,
-                        passwd: personInfo.passwd,
-                        passwdConf: personInfo.passwdConf,
-                        dob: personInfo.dob,
+						email: personInfo.email,
+						passwd: personInfo.passwd,
+						passwdConf: personInfo.passwdConf,
+						dob: personInfo.dob,
 						gender: personInfo.gender,
 						city: personInfo.city,
 						noHp: personInfo.noHp,
 						dateJoin: personInfo.dateJoin
-                    });
+					});
 
-                    newPerson.save(function(err, Person){
-                        if(err)
-                            res.json({status: "x ok"})
-                        else
-                            //res.json({status: "ok"})
-                            res.redirect('/employee');
-                    });
+					newPerson.save(function (err, Person) {
+						if (err)
+							res.json({
+								status: "x ok"
+							})
+						else
+							//res.json({status: "ok"})
+							res.redirect('/employee');
+					});
 
-                }
+				}
 
-            })
+			})
 
-        }else{
-            res.send({"Success1":"password is not matched"});
-        }
+		} else {
+			res.send({
+				"Success1": "password is not matched"
+			});
+		}
 
-    }
+	}
 });
 
 
@@ -177,47 +198,51 @@ router.post('/addEmp', function(req, res, next) { //registration
 router.get('/editEmp', function (req, res, next) {
 	//let id = req.params.unique_id;
 	console.log("Edit employee");
-	User.findOne({_id:req.session.userId},function(err,data){
+	User.findOne({
+		_id: req.session.userId
+	}, function (err, data) {
 		console.log("data");
 		console.log(data);
-		if(!data){
+		if (!data) {
 			res.redirect('/employee');
-		}else{
-	 		console.log("found");
-			return res.render('admin/employee.ejs' , {
-				"id":data.unique_id,
-				"name":data.name,
-				"email":data.email,
-				"passwd":data.passwd,
-				"dob":data.dob,
-				"gender":data.gender,
-				"city":data.city,
-				"noHp":data.noHp,
-				"dateJoin":data.dateJoin
-				
+		} else {
+			console.log("found");
+			return res.render('admin/employee.ejs', {
+				"id": data.unique_id,
+				"name": data.name,
+				"email": data.email,
+				"passwd": data.passwd,
+				"dob": data.dob,
+				"gender": data.gender,
+				"city": data.city,
+				"noHp": data.noHp,
+				"dateJoin": data.dateJoin
+
 			});
 		}
 	});
 });
 
-router.post('/editEmp', function(req, res, next) {
+router.post('/editEmp', function (req, res, next) {
 	// Create Mongose Method to Update a Existing Record Into Collection
-	
+
 	var data = {
-		name : req.body.name,
+		name: req.body.name,
 		phone: req.body.phone,
 		dob: req.body.dob,
 		city: req.body.city,
-		email:req.body.email
+		email: req.body.email
 	}
-		// Save User
-		User.findByIdAndUpdate({_id:req.session.userId}, data, function(err, docs) {
-			if (err) throw err
-			else{
-				console.log(docs);
-				res.redirect('/profile');
-			}
-	
+	// Save User
+	User.findByIdAndUpdate({
+		_id: req.session.userId
+	}, data, function (err, docs) {
+		if (err) throw err
+		else {
+			console.log(docs);
+			res.redirect('/profile');
+		}
+
 	});
 });
 
@@ -231,36 +256,37 @@ router.get('/setting', function (req, res, next) {
 
 
 const admin = {
-	email:"admin@gmail.com",
-	passwd:"admin123",	
+	email: "admin@gmail.com",
+	passwd: "admin123",
 }
 
-router.get('/adminIndex', function (req, res, next){
+router.get('/adminIndex', function (req, res, next) {
 	return res.render('admin/adminIndex.ejs');
-} );
+});
 
 
 
 // ------------------------------------- LOGIN ------------------------------------------
 
-router.post('/login', (req, res)=>{
-	
-	if(req.body.email == admin.email && req.body.passwd == admin.passwd){
+router.post('/login', (req, res) => {
+
+	if (req.body.email == admin.email && req.body.passwd == admin.passwd) {
 		req.session.user = req.body.email;
 		res.redirect('/adminIndex');
-	
-	}else
-	{
-		User.findOne({email:req.body.email},function(err,data){
-		if(data.email == req.body.email && data.passwd == req.body.passwd){
-			req.session.userId = data._id;
-			console.log("session = "+ req.session.userId);
-			res.redirect('/dashboard');
 
-    	}else{
-        res.end("Invalid Username")
-    	}
-	})
+	} else {
+		User.findOne({
+			email: req.body.email
+		}, function (err, data) {
+			if (data.email == req.body.email && data.passwd == req.body.passwd) {
+				req.session.userId = data._id;
+				console.log("session = " + req.session.userId);
+				res.redirect('/dashboard');
+
+			} else {
+				res.end("Invalid Username")
+			}
+		})
 	};
 });
 
@@ -309,151 +335,162 @@ router.post('/update', function(req, res, next) {
 	
 	});
 	});*/
-	//let id = req.params.unique_id;
-	
+//let id = req.params.unique_id;
+
 router.get('/logout', function (req, res, next) {
 	console.log("logout")
 	if (req.session) {
-    // delete session object
-    req.session.destroy(function (err) {
-    	if (err) {
-    		return next(err);
-    	} else {
-    		return res.redirect('/');
-    	}
-    });
-}
+		// delete session object
+		req.session.destroy(function (err) {
+			if (err) {
+				return next(err);
+			} else {
+				return res.redirect('/');
+			}
+		});
+	}
 });
 
 //-----------------------------------------------------Appointment---------------------------------------
-router.post("/booking",(req,res) => {
-    // validate request
-	
-    if(!req.body){
-        res.status(400).send({ message : "Content cannot be empty!"});
-        return;
-    }
+router.post("/booking", (req, res) => {
+	// validate request
 
-	User.findOne({_id:req.session.userId},function(err,data){
+	if (!req.body) {
+		res.status(400).send({
+			message: "Content cannot be empty!"
+		});
+		return;
+	}
+
+	User.findOne({
+		_id: req.session.userId
+	}, function (err, data) {
 		let id = data.unique_id;
 		const appointment = new Booking({
-		user_id : id,
-		user_name: data.name,
-		bookDate : req.body.date,
-		bookTime : req.body.time,
-		services: req.body.services,
-		status:"Approved"   
-    })
-        
-    // new booking
-    // save user in the database
-    appointment
-        .save(appointment)
-        .then(data => {
-            //res.send(data)
-            res.redirect('bookingInfo');
-        })
-        .catch(err =>{
-            res.status(500).send({
-                message : err.message || "Some error occurred while creating a create operation"
-            });
-        });
-	});
-	
-});
+			user_id: id,
+			user_name: data.name,
+			bookDate: req.body.date,
+			bookTime: req.body.time,
+			services: req.body.services,
+			status: "Approved"
+		})
 
-router.get("/bookingInfo",(req, res,)=>{
-	
-	User.findOne({_id:req.session.userId},function(err,data){
-	Booking.find({user_id:data.unique_id},(err, docs) => {
-        if (!err) {	
-            res.render("bookingInfo.ejs", {
-                data: docs
-            });	
-        } else {
-            console.log('Failed to retrieve the Users List: ' + err);
-        }
+		// new booking
+		// save user in the database
+		appointment
+			.save(appointment)
+			.then(data => {
+				//res.send(data)
+				res.redirect('bookingInfo');
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: err.message || "Some error occurred while creating a create operation"
+				});
+			});
 	});
 
 });
+
+router.get("/bookingInfo", (req, res, ) => {
+
+	User.findOne({
+		_id: req.session.userId
+	}, function (err, data) {
+		Booking.find({
+			user_id: data.unique_id
+		}, (err, docs) => {
+			if (!err) {
+				res.render("bookingInfo.ejs", {
+					data: docs
+				});
+			} else {
+				console.log('Failed to retrieve the Users List: ' + err);
+			}
+		});
+
+	});
 });
 
 
-router.get("/delete/:_id",(req, res,)=>{	
-    Booking.findByIdAndRemove(req.params._id, (err, doc) => {
-        if (!err) {
-            res.redirect('/bookingInfo');
-        } else {
-            console.log('Failed to Delete user Details: ' + err);
-        }
-    });
+router.get("/delete/:_id", (req, res, ) => {
+	Booking.findByIdAndRemove(req.params._id, (err, doc) => {
+		if (!err) {
+			res.redirect('/bookingInfo');
+		} else {
+			console.log('Failed to Delete user Details: ' + err);
+		}
+	});
 });
 // 
 
 
 //-------------------------------------------------------APPOINTMENT-------------------------------------------------------------------
- 
+
 //LIST BOOKING
 //------------------------------------------------ADMIN------------------------------------------------------------------------------
 
 
 
-router.get("/listBooking",(req, res,)=>{
+router.get("/listBooking", (req, res, ) => {
 
 	Booking.find((err, docs) => {
-        if (!err) {
-			if((req.session.user == admin.email)){
-				
-				return res.render('admin/listBooking.ejs', {
-					data:docs,
-				});	
-				}
-			 else if ((req.session.user == doctor.email)){
-					return res.render('admin/listBookingforDr.ejs', {
-						data:docs,
-					});	
-				}
-					
-            
-				
-                
-            //});
-        } else {
-            console.log('Failed to retrieve the Booking List: ' + err);
-        }
-    });
+		if (!err) {
+			if ((req.session.user == admin.email)) {
 
-	
-}); 
+				return res.render('admin/listBooking.ejs', {
+					data: docs,
+				});
+			} else if ((req.session.user == doctor.email)) {
+				return res.render('admin/listBookingforDr.ejs', {
+					data: docs,
+				});
+			}
+
+
+
+
+			//});
+		} else {
+			console.log('Failed to retrieve the Booking List: ' + err);
+		}
+	});
+
+
+});
 
 
 
 //updateListBooking Admin ----------------
 
-router.post('/updateAdmin/:_id', function(req, res, next) {
+router.post('/updateAdmin/:_id', function (req, res, next) {
 	// Create Mongose Method to Update a Existing Record Into Collection
 
-		// Save User
-		Booking.findByIdAndUpdate({_id:req.params._id}, {doctor: req.body.doctor}, function(err, docs) {
-			if (err) throw err
-			else{
-				console.log(docs);
-				res.redirect('/listBooking');
-			}
-	
+	// Save User
+	Booking.findByIdAndUpdate({
+		_id: req.params._id
+	}, {
+		doctor: req.body.doctor
+	}, function (err, docs) {
+		if (err) throw err
+		else {
+			console.log(docs);
+			res.redirect('/listBooking');
+		}
+
 	});
-	}); 
+});
 
 
-router.get("/deleteAdmin/:_id",(req, res,)=>{
-    Booking.findByIdAndRemove(req.params._id, (err, doc) => {
+router.get("/deleteAdmin/:_id", (req, res, ) => {
+	Booking.findByIdAndRemove(req.params._id, (err, doc) => {
 		let id = doc.user_id;
-        if (!err) {
-            res.redirect('/listBooking');
-        } else {
-            console.log('Failed to Delete user Details: ' + err);
-        }
- });
+		if (!err) {
+			res.redirect('/listBooking');
+		} else {
+			console.log('Failed to Delete user Details: ' + err);
+		}
+	});
 });
 
 //Patient Record-----------------
@@ -469,44 +506,45 @@ router.get('/services2', function (req, res, next) {
 
 
 //LIST OF PATIENT ---------------------------
-router.get("/listPatient",(req, res,)=>{
+router.get("/listPatient", (req, res, ) => {
 
 	User.find((err, docs) => {
-        if (!err) {
-            res.render("admin/listPatient.ejs", {
+		if (!err) {
+			res.render("admin/listPatient.ejs", {
 				data: docs,
-            });
-        } else {
-            console.log('Failed to retrieve the Patient List: ' + err);
-        }
-    });
+			});
+		} else {
+			console.log('Failed to retrieve the Patient List: ' + err);
+		}
+	});
 
-	
-}); 
 
-router.get("/patientRecord/:unique_id",(req, res,)=>{
-		Booking.find({user_id:req.params.unique_id},(err, document) => {
-			if (!err) {
-				res.render('admin/patientRecord.ejs', {
-					data: document
-					
-				});
-			} else {
-				console.log('Failed to retrieve the Booking List: ' + err);
-			}
-		});
+});
+
+router.get("/patientRecord/:unique_id", (req, res, ) => {
+	Booking.find({
+		user_id: req.params.unique_id
+	}, (err, document) => {
+		if (!err) {
+			res.render('admin/patientRecord.ejs', {
+				data: document
+
+			});
+		} else {
+			console.log('Failed to retrieve the Booking List: ' + err);
+		}
+	});
 });
 
 //-----------------------------------------------------ADMIN---------------------------------------
 
 
 router.get('/booking', function (req, res, next) {
-	if(req.session.userId){
-	return res.render('booking.ejs');
-}
-else{
-	res.send("Please Login First For Booking ");
-}
+	if (req.session.userId) {
+		return res.render('booking.ejs');
+	} else {
+		res.send("Please Login First For Booking ");
+	}
 });
 
 router.get('/editprofile', function (req, res, next) {
@@ -526,13 +564,12 @@ router.get('/bookingInfo', function (req, res, next) {
 });
 
 router.get('/listBooking', function (req, res, next) {
-	if((req.session.user = "admin@gmail.com")){
-	return res.render('admin/listBooking.ejs');
-	}
-	else{
+	if ((req.session.user = "admin@gmail.com")) {
+		return res.render('admin/listBooking.ejs');
+	} else {
 		return res.render('admin/listBookingforDr.ejs');
 	}
-	
+
 });
 
 router.get('/patientRecord', function (req, res, next) {
@@ -551,4 +588,3 @@ router.get('/listPatient', function (req, res, next) {
 
 
 module.exports = router;
-
